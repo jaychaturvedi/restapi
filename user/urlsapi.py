b523da2e86519fb9eee87804e09b53f8ce2fcfe6
jaychaturvedi <jaychaturvedi18@gmail.com>
2019-10-14 00:44:31 +0530

    added test_token_for_user

--- user/urlsapi.py
+++ user/urlsapi.py
@@ -14,4 +14,5 @@ app_name = 'user'
 
 urlpatterns = [
     path('create/',CreateUserView.as_view(), name='create'),
+    path('token/',CreateTokenView.as_view(), name='token'),
 ]
--- user/views.py
+++ user/views.py
@@ -13,14 +13,22 @@ from rest_framework import viewsets
 
 #restAPI -- class viewset
 from rest_framework import generics
-from user.serializers import UserSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+from user.serializers import UserSerializer, AuthTokenSerializer
 User = get_user_model()
 
+
+
+class CreateTokenView(ObtainAuthToken):
+    """Create a new auth token for the user"""
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
 
 class CreateUserView(generics.CreateAPIView):
     """Create a new user in the system"""
     serializer_class = UserSerializer
-    queryset = User.objects.all()
+
 
 
 # class UserViewSet(viewsets.ModelViewSet):
